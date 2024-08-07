2e8ea10e6581521bca755c1eb008542a580f8337
Paul Kasay <paul@cloverly.com>
2024-08-06 22:32:41 -0400

    add csv download

--- GenerateCSV/__init__.py
+++ GenerateCSV/__init__.py
@@ -2,9 +2,6 @@ from GenerateCSV.main import *
 import azure.functions as func
 import logging
 
-
-# deals = fetch_deals('2024-07-18','2024-07-20')
-
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     print("Main triggered")
@@ -17,21 +14,18 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     req_body = req.get_json()
     start_date = req_body.get('start_date', None)
     end_date = req_body.get('end_date', None)
-    initial_csv = req_body.get('initial_csv', None)
-    final_csv = req_body.get('final_csv', None)
 
-    if not start_date:
-        # 3 months etc
-
+    # Generate the CSV in-memory
     try:
-        result =
-        # return func.HttpResponse(result, status_code=200)
-        if result.status_code == 200:
-            return func.HttpResponse(f"Main 1 - Processed - {result.status_code}", status_code=200)
-        elif result.status_code == 500:
-            return func.HttpResponse(f"Main 0 Failed to Process - {result.get_body()} -{result.status_code}", status_code=500)
-        else:
-            return func.HttpResponse("Main 0 Failed to Process - ")
+        csv_output, filename = export_csv(start_date, end_date)
+        # Return the CSV as an HTTP response with the appropriate headers
+        return func.HttpResponse(
+            csv_output.getvalue(),
+            mimetype="text/csv",
+            headers={
+                "Content-Disposition": f"attachment; filename={filename}"
+            }
+        )
     except Exception as e:
         logging.error(f"Main exception found: {e}")
         return func.HttpResponse(str(e), status_code=500)
